60892822c0e03184ea262290a32c591bc03bdff5
Anupa Gamage <anupakavishka@gmail.com>
2024-05-29 11:50:48 +0530

    Update ImageCard.jsx

--- src/ImageCard.jsx
+++ src/ImageCard.jsx
@@ -1,31 +1,17 @@
 import React from 'react'
 
 
-const ImageCard = () => {
+const ImageCard = ({img, title, desc,}) => {
   return (
     <div className="container">
             <div className="card">
                 <div className="images">
-                    <img src="pic1.png" alt=""/>
-                    <h1>Nike Shoes</h1>
+                    <img src={img} alt=""/>
+                    <h1>{title}</h1>
                 </div>
                 
                 <div className="content">
-                    <div className="size">
-                        <h3>Size :</h3>
-                        <span>7</span>
-                        <span>8</span>
-                        <span>9</span>
-                        <span>10</span>
-                    </div>
-
-                    <div className="color">
-                        <h3>Color :</h3>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                    </div>
-                    <a href="#">Buy Now</a>
+                   <p>{desc}</p>
                 </div>
             </div>
 
